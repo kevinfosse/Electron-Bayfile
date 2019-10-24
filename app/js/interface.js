a3830f53f55b45adbe1f3805388edc1d3a12f7b5
Pharuxtan <alex360noscop@gmail.com>
2019-10-24 21:04:09 +0200

    Add files via upload

--- app/js/interface.js
+++ app/js/interface.js
@@ -49,7 +49,8 @@ function app() {
     if(file === null) return document.getElementById("select").setAttribute("value", "Please select a file !");
     isuploading = true;
     document.getElementById("progress").setAttribute("style", "visibility: visible;");
-    console.log("req")
+    console.log("req");
+    /*console.log(path.join(__dirname, "bin", `curl${(process.platform === "win32") ? ".exe" : ""}`))
     let child = exec(`"${path.join(__dirname, "bin", `curl${(process.platform === "win32") ? ".exe" : ""}`)}" -# -F "file=@${file}" https://api.bayfiles.com/upload`);
     child.stdout.on("data", data => {
       data = JSON.parse(data);
@@ -72,7 +73,72 @@ function app() {
         document.getElementById("upload").setAttribute("value", pourcent + "%");
         document.getElementById("progress").setAttribute("value", pourcent);
       }
+    });*/
+
+    function genBoundary() {
+      var boundary = '------------------------';
+      for (var i = 0; i < 16; i++) {
+        boundary += Math.floor(Math.random() * 10).toString(16);
+      }
+      return boundary;
+    }
+
+    const fs = require('fs')
+    //const util = require('util')
+    const HttpsProxyAgent = require('https-proxy-agent');
+    const axios = require('axios');
+    const FormData = require('form-data');
+    const fetch = require("node-fech");
+    const Progress = require('node-fetch-progress');
+
+    const fileStream = fs.createReadStream(file);
+    const fileStats = fs.statSync(file);
+    const fileLength = fileStats.size;
+    const form = new FormData()
+    form.append('file', fileStream, { knownLength: fileLength });
+
+    /*axios.post('https://api.bayfiles.com/upload', form, {
+      headers: form.getHeaders(),
+      proxy: {
+        host: '127.0.0.1',
+        port: 8888
+      }
+    }).then(result => {
+      console.log(result);
     });
+
+    /*var xhr = new XMLHttpRequest();
+    xhr.open('POST', "https://api.bayfiles.com/upload", true);
+    xhr.upload.addEventListener("progress", function(evt) {
+      if (evt.lengthComputable) {
+        var percentComplete = evt.loaded / evt.total;
+        percentComplete = parseInt(percentComplete * 100);
+        console.log(percentComplete);
+
+        if (percentComplete === 100) {
+          console.log(evt)
+        }
+
+      }
+    }, false);
+    xhr.onload = function () {
+      console.log(JSON.parse(xhr.response));
+    };
+    xhr.send(form);
+
+    /*process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    let res = await fetch("https://api.bayfiles.com/upload", {
+      method: 'POST',
+      headers: form.getHeaders(),
+      body: form
+    });
+
+    if (!res.ok && res.status !== 400) {
+      console.log("hmm");
+    } else {
+      const { status, error, data } = await res.json()
+      console.log(data);
+    }*/
   });
   $("#linkafterupload").click(function() {
     console.log('click');
